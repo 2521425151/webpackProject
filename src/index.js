8ca98e81c4992a684442d0b945ddb6014e0ee200
hebaogan <2521425151@qq.com>
2019-09-30 11:26:24 +0800

    webpack 加载字体&加载图片

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import _ from 'lodash'
 import './style.css'
+import WPPath from './WP.png'
 function component(){
     var element = document.createElement('div')
     element.innerHTML = _.join(['Hello','webpack'],' ')
     element.classList.add('hello')
+
+    // 将图像添加到我们现有的div
+    var myIcon = new Image()
+    myIcon.src = WPPath
+    element.appendChild(myIcon)
     return element
 }
 document.body.appendChild(component())
