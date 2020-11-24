d8469310808a7b85f818868c4a76a3cdc7485f2b
hebaogan <2521425151@qq.com>
2020-11-24 19:52:30 +0800

    feat: 安装webpack-dev-server，搭建开发环境的热监测服务器

---
+++ README.md
@@ -0,0 +1,3 @@
+###### webpack-dev-server 和 webpack-cli 版本会冲突请使用下方版本
+“webpack-cli": "^3.3.12",
+"webpack-dev-server": "^3.11.0"
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,14 @@ module.exports = {
         index: './src/js/index.js',
         login: './src/js/login.js'
     },
+    // 动态监测并实时更新页面
+    devServer: {
+        contentBase: './dist',
+        // 默认8080.可不写
+        port: 8080,
+        // 热更新，无需刷新
+        hot: true
+    },
     plugins: [
         // 自动清空dist目录
         new CleanWebpackPlugin(),
