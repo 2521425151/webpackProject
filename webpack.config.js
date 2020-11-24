8111385328fcd75137f8637a8a41197c547d050d
hebaogan <2521425151@qq.com>
2020-11-24 16:13:37 +0800

    feat: 搭建基础架构 1、安装clean-webpack-plugin，实现每次编译前自动清空dist目录；2、安装html-webpack-plugin，实现从html模板自动生成最终html

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,28 @@
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        index: './src/js/index.js',
+        login: './src/js/login.js'
+    },
+    plugins: [
+        // 自动清空dist目录
+        new CleanWebpackPlugin(),
+        // 设置html模板生成路径
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/html/index.html',
+            chunks: ['index']
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'login.html',
+            template: './src/html/login.html',
+            chunks: ['login']
+        }),
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: 'js/[name].js',
         path: path.resolve(__dirname, 'dist')
     }
 };
