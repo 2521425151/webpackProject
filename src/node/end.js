eab5308d2ac7d12993c1a054159f5c5aeb9e73d5
hebaogan <2521425151@qq.com>
2021-01-18 22:01:19 +0800

    feat: 打包css

--- src/node/end.js
+++ src/node/end.js
@@ -1,1 +1,2 @@
+import "./index.css"
 console.log("webpack log node end 1"); // 打印
--- webpack.common.js
+++ webpack.common.js
@@ -19,6 +19,10 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.css$/,   // 正则表达式，表示.css后缀的文件
+                use: ['style-loader','css-loader']   // 针对css文件使用的loader，注意有先后顺序，数组项越靠后越先执行
+            },
+            {
                 test: /\.js$/,
                 exclude: /(node_modules)/,
                 use: {
